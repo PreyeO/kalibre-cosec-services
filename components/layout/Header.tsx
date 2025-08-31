af983a144f81e4dd104b46ddb501f28b057574f9
PreyeO <omusukup@yahoo.com>
2025-08-31 02:58:55 +0100

    fixes

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="mt-[46px] md:mt-0 w-full bg-[#122847] flex md:justify-start justify-center  h-[30px] md:h-[41px] backdrop-blur-[15px] text-white px-5 md:px-[50px]">
-      <div className="flex items-center gap-5 font-secondary font-medium text-[12px]">
+      <div className="flex items-center gap-3 md:gap-5 font-secondary font-medium text-[12px]">
         <div className="flex items-center gap-[3px]">
           <Phone size={18} />
           <p className="">+2348028890404</p>
