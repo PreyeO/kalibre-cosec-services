1fa40d8fe22b47fa18af2ad1303a9fda127af0f2
PreyeO <omusukup@yahoo.com>
2025-09-02 13:58:25 +0100

    changes

--- components/ui/buttons/floating-call-button.tsx
+++ components/ui/buttons/floating-call-button.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { PhoneCall } from "lucide-react";
-import Link from "next/link";
 import {
   Tooltip,
   TooltipTrigger,
@@ -12,12 +11,16 @@ export default function FloatingCallButton() {
   return (
     <Tooltip>
       <TooltipTrigger asChild>
-        <Link
-          href="tel:+2348028890404"
-          className="fixed bottom-6 right-6 z-50 bg-[#F67D30] cursor-pointer md:w-15 md:h-15 w-12 h-12 rounded-full shadow-lg flex items-center justify-center"
+        <a
+          href="https://forms.gle/WCN4tKnrT39WTQY27"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fixed bottom-6 right-6 z-50 bg-[#F67D30] cursor-pointer
+          md:w-15 md:h-15 w-12 h-12 rounded-full shadow-lg flex items-center
+          justify-center"
         >
           <PhoneCall className="md:w-6 md:h-6 w-5 h-5 text-white" />
-        </Link>
+        </a>
       </TooltipTrigger>
       <TooltipContent
         side="top"
