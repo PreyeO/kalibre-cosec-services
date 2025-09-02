5983f41c09cc5b53ef9c9020e182e661dacd773b
PreyeO <omusukup@yahoo.com>
2025-09-02 12:52:14 +0100

    updates

--- components/ui/buttons/floating-call-button.tsx
+++ components/ui/buttons/floating-call-button.tsx
@@ -16,10 +16,16 @@ export default function FloatingCallButton() {
           href="tel:+2348028890404"
           className="fixed bottom-6 right-6 z-50 bg-[#F67D30] cursor-pointer md:w-15 md:h-15 w-12 h-12 rounded-full shadow-lg flex items-center justify-center"
         >
-          <PhoneCall className="w-6 h-6 text-white" />
+          <PhoneCall className="md:w-6 md:h-6 w-5 h-5 text-white" />
         </Link>
       </TooltipTrigger>
-      <TooltipContent side="top" sideOffset={6} className="p-6 bg-[#122847]">
+      <TooltipContent
+        side="top"
+        sideOffset={6}
+        align="end"
+        alignOffset={20}
+        className="bg-[#122847] text-white px-3 py-4 rounded-md shadow-md animate-in fade-in zoom-in-95 slide-in-from-right-2"
+      >
         Book a discovery call
       </TooltipContent>
     </Tooltip>
